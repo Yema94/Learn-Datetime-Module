d258902b2f86625dc7221c664fb6b1b729847eb5
Yema <myr29893@gmail.com>
2021-05-27 13:13:56 +0200

    Can handle multiple tasks, resources and projects now

--- projectmanagement.py
+++ projectmanagement.py
@@ -1,3 +1,8 @@
+projectslist = []
+def addproject(project):
+    projectslist.append(project)
+
+
 class Project:
     def __init__(self, name, strdate, stopdate):
         self.name = name
@@ -25,14 +30,35 @@ class Resource:
         self.skill = skill
 
 
+
+
+
 projectcobj = Project("Name of Project one", "Starting Date: 1st June 2021", "Ending Date: 1st Sept 2021")
-taskcobj = Task("Name of Task one of project one", "Duration: 20 Days")
-resourcecobj = Resource("Name of Person one working on project one", "Python Specialist")
-task.addResource(resourcecobj)
-project.addTask(taskcobj)
 
 
-for eachTaskobj in project.taskslist:
-    print(eachTaskobj.name, eachTaskobj.duration)
-    for eachResourceobj in eachTaskobj.resourceslist:
-        print(eachResourceobj.name, eachResourceobj.skill)
+taskcobj1 = Task("Name of Task one of project one", "Duration: 20 Days")
+
+resourcecobj1 = Resource("Name of Person one working on project one", "Python Specialist")
+resourcecobj2 = Resource("Name of Person two working on project one", "Data Science Specialist")
+
+taskcobj1.addResource(resourcecobj1)
+taskcobj1.addResource(resourcecobj2)
+
+projectcobj.addTask(taskcobj1)
+
+taskcobj2 = Task("Name of Task two of project one", "Duration: 20 Days")
+
+taskcobj2.addResource(resourcecobj1)
+
+projectcobj.addTask(taskcobj2)
+
+
+addproject(projectcobj)
+
+
+for eachprojectobj in projectslist:
+    print(eachprojectobj.name, eachprojectobj.strdate, eachprojectobj.stopdate)
+    for eachTaskobj in projectcobj.taskslist:
+        print(" ",eachTaskobj.name, eachTaskobj.duration)
+        for eachResourceobj in eachTaskobj.resourceslist:
+            print("     ",eachResourceobj.name, eachResourceobj.skill)
