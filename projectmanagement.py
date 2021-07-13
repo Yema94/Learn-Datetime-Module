cd2bb452447faf723075fff72e3701157e9ca55c
Yeshwanth Manasani <63667600+Yema94@users.noreply.github.com>
2021-07-13 19:38:37 +0200

    Manages multiple projects, tasks and resources
    # Following tree diagram shows what this project management code tries to do.. 
     
    
    body {
      font-family: sans-serif;
      font-size: 15px;
    }
    
    $border-width: 1px;
    $reverse: false;
    
    .tree {
      @if $reverse {
        transform: rotate(180deg);
        transform-origin: 50%;
      }
    }
    
    .tree ul {
      position: relative;
    	padding: 1em 0; 
      white-space: nowrap;
      margin: 0 auto;
      text-align: center;
      &::after {
        content: '';
        display: table;
        clear: both;
      }
    }
    
    .tree li {
      display: inline-block; // need white-space fix
      vertical-align: top;
      text-align: center;
    	list-style-type: none;
    	position: relative;
    	padding: 1em .5em 0 .5em;
      &::before,
      &::after {
        content: '';
        position: absolute; 
        top: 0; 
        right: 50%;
        border-top: $border-width solid #ccc;
        width: 50%; 
        height: 1em;
      }
      &::after {
        right: auto; 
        left: 50%;
    	  border-left: $border-width solid #ccc;
      }
      &:only-child::after,
      &:only-child::before {
        display: none;
      }
      &:only-child {
        padding-top: 0;
      }
      &:first-child::before,
      &:last-child::after {
        border: 0 none;
      }
      &:last-child::before{
        border-right: $border-width solid #ccc;
        border-radius: 0 5px 0 0;
      }
      &:first-child::after{
        border-radius: 5px 0 0 0;
      }
    }
    
    .tree ul ul::before{
    	content: '';
    	position: absolute; 
      top: 0; 
      left: 50%;
    	border-left: $border-width solid #ccc;
    	width: 0; 
      height: 1em;
    }
    
    .tree li a {
    	border: $border-width solid #ccc;
    	padding: .5em .75em;
    	text-decoration: none;
    	display: inline-block;
    	border-radius: 5px;
      color: #333;
      position: relative;
      top: $border-width;
      @if $reverse {
        transform: rotate(180deg);
      }
    }
    
    .tree li a:hover,
    .tree li a:hover+ul li a {
    	background: #e9453f;
      color: #fff;
      border: $border-width solid #e9453f;
    }
    
    .tree li a:hover + ul li::after, 
    .tree li a:hover + ul li::before, 
    .tree li a:hover + ul::before, 
    .tree li a:hover + ul ul::before{
    	border-color:  #e9453f;
    }
    
    <div class="tree">
    	<ul>
      <li>
        <a href="#">Project-1</a>
        <ul>
          <li>
            <a href="#">Task-1</a>
            <ul>
              <li>
                <a href="#">Resource-1</a>
              </li><li>
                <a href="#">Resource-2</a>
                <ul>
                  <li>
                    <a href="#">Skill-1</a>
                  </li><li>
                    <a href="#">Skill-2</a>
                  </li>
                </ul>
              </li>
            </ul>
          </li><li>
            <a href="#">Task-2</a>
            <ul>
              <li>
                <a href="#">Resource-1</a>
                <ul>
                  <li>
                    <a href="#">Skill-1</a>
                  </li>
                </ul>
              </li><li>
                <a href="#">Resource-2</a>
              </li>
            </ul>
          </li>
        </ul>
      </li><li>
        <a href="#">Project-2</a>
        <ul>
          <li>
            <a href="#">Task-1</a>
            <ul>
              <li>
                <a href="#">Resource-1</a>
              </li><li>
                <a href="#">Resource-2</a>
                <ul>
                  <li>
                    <a href="#">Skill-1</a>
                  </li><li>
                    <a href="#">Skill-2</a>
                  </li>
                </ul>
              </li>
            </ul>
          </li><li>
            <a href="#">Task-2</a>
            <ul>
              <li>
                <a href="#">Resource-1</a>
                <ul>
                  <li>
                    <a href="#">Skill-1</a>
                  </li>
                </ul>
              </li><li>
                <a href="#">Resource-2</a>
              </li>
            </ul>
          </li>
        </ul>
      </li>
    </ul>

--- projectmanagement.py
+++ projectmanagement.py
@@ -1,64 +1,96 @@
+# To manage multiple projects, tasks in those projects, and resources alloced for those tasks
+
+
+# stores the objects of the project class
+# len(projectslist) tells the number of projects
 projectslist = []
+
 def addproject(project):
+    """This function takes the the obj of the project class as a paramenter and adds to the "projectlist"
+     here number of objs == number of projects"""
     projectslist.append(project)
 
-
+    
+# ============================================Project Class============================================
 class Project:
+    """This Project class obj takes the name, startdate, and enddate of the project as initializing parameters """
+    
     def __init__(self, name, strdate, stopdate):
-        self.name = name
-        self.strdate = strdate
-        self.stopdate = stopdate
-        self.taskslist = []
+        self.name = name # name of the project
+        self.strdate = strdate # start date of the project
+        self.stopdate = stopdate # end date of the project
+        
+        # stores the objs of the tasks class related to that particular project
+        # len(taskslist) == number of Task class objs == number of tasks in that particular project
+        self.taskslist = [] 
 
     def addTask(self, task):
+    """This function takes the the obj of the Task class as a paramenter and adds to the "taskslist""""
         self.taskslist.append(task)
+        
+    def __del__(self):
+        print("Destruction of Project Object!")
 
-
+        
+# ============================================Task Class============================================
 class Task:
+    """This Task class obj takes the name, and duration of the task as initializing parameters """    
     def __init__(self, name, duration):
-        self.name = name
-        self.duration = duration
-        self.resourceslist = []
+        self.name = name # name of the task
+        self.duration = duration # time duration of the task 
+        
+        # stores the objs of the resources allocated to this particular task
+        # len(resourcelist) == number of resource class objs == number of persons working on that task
+        self.resourceslist = [] 
 
     def addResource(self, resource):
+        """This function takes the the obj of the Resource class as a paramenter and adds to the "resourceslist""""
         self.resourceslist.append(resource)
+      
+    def __del__(self):
+        print("Destruction of Task Object!")
 
-
+ 
+# ============================================Resource Class============================================
 class Resource:
+    """This Resource class obj takes the name, and skill of the employee allocated on a particular task as initializing parameters """  
     def __init__(self, name, skill):
-        self.name = name
-        self.skill = skill
+        self.name = name # name of the employee
+        self.skill = skill # skill set of this employee
+        
+    def __del__(self):
+        print("Destruction of Resource Object!")        
 
 
+# ============================================Main Program============================================        
+if __name__=="__main__":
+
+    projectcobj = Project("Name of Project one", "Starting Date: 1st June 2021", "Ending Date: 1st Sept 2021")
 
 
+    taskcobj1 = Task("Name of Task one of project one", "Duration: 20 Days")
 
-projectcobj = Project("Name of Project one", "Starting Date: 1st June 2021", "Ending Date: 1st Sept 2021")
+    resourcecobj1 = Resource("Name of Person one working on project one", "Python Specialist")
+    resourcecobj2 = Resource("Name of Person two working on project one", "Data Science Specialist")
 
+    taskcobj1.addResource(resourcecobj1)
+    taskcobj1.addResource(resourcecobj2)
 
-taskcobj1 = Task("Name of Task one of project one", "Duration: 20 Days")
+    projectcobj.addTask(taskcobj1)
 
-resourcecobj1 = Resource("Name of Person one working on project one", "Python Specialist")
-resourcecobj2 = Resource("Name of Person two working on project one", "Data Science Specialist")
+    taskcobj2 = Task("Name of Task two of project one", "Duration: 20 Days")
 
-taskcobj1.addResource(resourcecobj1)
-taskcobj1.addResource(resourcecobj2)
+    taskcobj2.addResource(resourcecobj1)
 
-projectcobj.addTask(taskcobj1)
-
-taskcobj2 = Task("Name of Task two of project one", "Duration: 20 Days")
-
-taskcobj2.addResource(resourcecobj1)
-
-projectcobj.addTask(taskcobj2)
+    projectcobj.addTask(taskcobj2)
 
 
-addproject(projectcobj)
+    addproject(projectcobj)
 
 
-for eachprojectobj in projectslist:
-    print(eachprojectobj.name, eachprojectobj.strdate, eachprojectobj.stopdate)
-    for eachTaskobj in projectcobj.taskslist:
-        print(" ",eachTaskobj.name, eachTaskobj.duration)
-        for eachResourceobj in eachTaskobj.resourceslist:
-            print("     ",eachResourceobj.name, eachResourceobj.skill)
+    for eachprojectobj in projectslist:
+        print(eachprojectobj.name, eachprojectobj.strdate, eachprojectobj.stopdate)
+        for eachTaskobj in projectcobj.taskslist:
+            print(" ",eachTaskobj.name, eachTaskobj.duration)
+            for eachResourceobj in eachTaskobj.resourceslist:
+                print("     ",eachResourceobj.name, eachResourceobj.skill)
