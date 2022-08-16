84683c713fc88c455368f1370b0ac8af721ee3ce
Yema94 <myr29893@gmail.com>
2022-08-16 10:42:17 +0000

    Added Output format comments

--- dtdemo.py
+++ dtdemo.py
@@ -1,10 +1,14 @@
 import time
 import datetime
+
+# prints in seconds
 epochseconds = time.time()
 print(epochseconds)
 
+# output format: day month date hour:min:sec year
 t = time.ctime(time.time())
 print(t)
 
+# output format : year-month-date hour:min:sec.milliseconds
 dt = datetime.datetime.today()
 print(dt)
